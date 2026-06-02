e345dc88bcfdf4e0d4f8fde79e63f135801e22e3
Sanvi Rai <sanvirai098@gmail.com>
2026-06-02 21:43:37 +0530

    Rename _init_.py to __init__.py

--- test_connection.py
+++
@@ -1,8 +0,0 @@
-from bot.client import client
-
-try:
-    info = client.futures_account_balance()
-    print("Connected Successfully!")
-    print(info)
-except Exception as e:
-    print("Error:", e)
